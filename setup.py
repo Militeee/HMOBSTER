a9e250a10798874d55d90e4def28aecf5a6e0c44
Salvatore Milite <militesalvatore@gmail.com>
2023-02-08 11:33:26 +0100

    new_version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 #        return f.read()
 
 setup(name='HMOBSTER',
-      version='0.0.43',
+      version='0.0.44',
       description='VAF clustering for multiple karyotypes',
       url='https://github.com/Militeee/hmobster',
       author='Salvatore Milite',
